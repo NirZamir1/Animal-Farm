009968285c8f507eb68d543f998639e735f5c687
NirZamir <nirozamir@gmail.com>
2022-04-23 21:24:19 +0300

    HashSet implemented

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string txt = File.ReadAllText(@"C:\Users\PC\OneDrive\שולחן העבודה\AnimalFarm.txt");
+HashSet<string> WordsSet = new HashSet<string>();
+WordsSet.Add("yes");
+WordsSet.Add("has");
+WordsSet.Add("then");
+WordsSet.Add("the");
 var CharArray = txt.Where(x => x != '.' && x != ',' && x != ':' && x != ';' && x != '?' && x != '"' && x != '-' && x != '!' && x!= '\r').ToArray();
 string book = new string(CharArray);
-var WordsList = book.Split(' ', '\n').Where(x =>  x.Length > 2 && x.ToLower() != "was" && x.ToLower() != "were" && x.ToLower() != "yes" && x.ToLower() != "the" && x.ToLower() != "and" && x.ToLower()  != "not" && x.ToLower() != "had"  && x.ToLower() != "that"
-&& x.ToLower() != "them" && x.ToLower() != "their").GroupBy(x => x);
+//var WordsList = book.Split(' ', '\n').Where(x =>  x.Length > 2 && x.ToLower() != "was" && x.ToLower() != "were" && x.ToLower() != "yes" && x.ToLower() != "the" && x.ToLower() != "and" && x.ToLower()  != "not" && x.ToLower() != "had"  && x.ToLower() != "that"
+//&& x.ToLower() != "them" && x.ToLower() != "their").GroupBy(x => x);
+var WordsList = book.Split(' ', '\n').Where(x=> WordsSet.All(z=> x.ToLower() != z.ToLower()) && x.Length > 2).GroupBy((x) => x);
 var TopWords = WordsList.OrderByDescending(x => x.Count()).Select(x => new { Word = x.ElementAt(0), Length = x.Count()}).Take(10);
 foreach (var wordList in TopWords)
 {
